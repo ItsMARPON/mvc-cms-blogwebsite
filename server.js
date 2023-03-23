f0a49e753f661c212769d0be4dd5858777b123ae
Mary <itsmaryyang@gmail.com>
2023-03-23 13:36:06 -0500

    Add code for the server.js for session and handlebars

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
 const path = require("path");
 const express = require("express");
+const session = require("express-session");
 const exphbs = require("express-handlebars");
+const routes = require("./controllers");
+const helpers = require("./utils/auth");
 
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require("./config/connection");
+// Handlebars.js engine creating and using custom helpers
+const hbs = exphbs.create({ helpers });
 
-const hbs = exphbs.create({});
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: "strict",
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
+app.use(session(sess));
+
+// Handlebars template being used in Express.js
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
@@ -16,7 +38,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(require("./controllers/"));
+// The routes for api and blog, comment, user routes
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () =>
