42955ce34761733fdb44daca7fac429860052c13
Mary <itsmaryyang@gmail.com>
2023-03-23 16:51:36 -0500

    add code to Comment route

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -14,6 +14,17 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.put('/:id', async (req, res)=> {
+  try{
+    await Comment.update(req.body, {
+      where: {id: req.params.id},
+    });
+
+    res.status(200).json({sucess: true});
+  } catch (err) {
+    res.status(404).json(err);
+  }
+});
 
 router.delete('/:id', withAuth, async(req, res)=> {
     try{
