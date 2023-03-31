2aab816c800c9605644b4cf654f6f44c002c472c
Mary <itsmaryyang@gmail.com>
2023-03-30 20:31:48 -0500

    Fix Comment seed data

--- seeds/commentData.js
+++ seeds/commentData.js
@@ -2,19 +2,19 @@ const {Comment} = require("../models");
 
 const commentData = [
 {
-    "comment_text": "Wow that was an awesome read!",
-    "blog_id": 1,
-    "user_id": 1
+    comment_text: "Wow that was an awesome read!",
+    blog_id: 1,
+    user_id: 1
 },
 {
-    "comment_text": "I can't wait to use MySQL2!",
-    "blog_id": 2,
-    "user_id": 2
+    comment_text: "I can't wait to use MySQL2!",
+    blog_id: 2,
+    user_id: 2
 },
 {
-    "comment_text": "Whoa, can't believe MySQL2 is so robust!",
-    "blog_id": 2,
-    "user_id": 2
+    comment_text: "Whoa, can't believe MySQL2 is so robust!",
+    blog_id: 2,
+    user_id: 2
 },
 ];
 
